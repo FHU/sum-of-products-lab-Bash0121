a190b6b775d79d6037b009b28427cab5bc40e5a3
Bash0121 <Robert.smith@students.fhu.edu>
2024-01-30 12:58:45 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
 #REMOVE PASS AND FIX THE FUNCTION
 def sum_of_products(list1, list2):
-        int_list1=[]
-        int_list2=[]
         list3=[]
-        for num1 in list1:
-              int_list1.append(int(list1))
-        for num2 in list2:
-              int_list2.append(int(list1))
-        for num3 in range(int_list1):
-              for num4 in range(int_list2):
+        list1 = [int(item) for item in list1]
+        list2 = [int(item) for item in list2]
+        for num3 in range(list1):
+              for num4 in range(list2):
                     list3.append(num3*num4)
                     return list3
 
@@ -19,4 +15,3 @@ if __name__ == '__main__':
    list2=list[input()]
    sum_of_products(list1, list2)
    print (sum(sum_of_products))
-   
