22753a2d7d83039d92eb2029d8dfc1429dfd0ebe
Bash0121 <Robert.smith@students.fhu.edu>
2024-01-31 14:23:54 -0600

    Update Main.py
    Update

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ def sum_of_products(list1, list2):
      for i in range(0, len(list2), 2):
          product=int(list1[i])*int(list2[i])
          total+=product
-         return product
+         return total
         
 if __name__ == '__main__':
    #REMOVE PASS AND YOUR CODE GOES HERE
    list1=input()
    list2=input()
-sum_of_products(list1, list2)
-print(sum_of_products)
+total=sum_of_products(list1, list2)
+print(total)
