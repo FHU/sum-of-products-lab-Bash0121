f3b1a3a2716923e7cd39d592f9d5f7381e7a3860
Bash0121 <Robert.smith@students.fhu.edu>
2024-01-31 14:31:36 -0600

    Update Main.py
    update 2

--- main.py
+++ main.py
@@ -2,14 +2,18 @@
 #change to test push and forks
 def sum_of_products(list1, list2):
      total=0
-     for i in range(0, len(list2), 2):
+     for i in range(len(list1)):
          product=int(list1[i])*int(list2[i])
          total+=product
          return total
         
 if __name__ == '__main__':
    #REMOVE PASS AND YOUR CODE GOES HERE
-   list1=input()
-   list2=input()
+   list1=[]
+   list2=[]
+   u_input1=input()
+   u_input2=input()
+   list1=u_input1.split()
+   list2=u_input2.split()
 total=sum_of_products(list1, list2)
 print(total)
